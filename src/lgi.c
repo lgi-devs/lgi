8bb2ea07a45773ef4c1ee5de8311c59ab1e68b27
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-07-19 14:38:52 +0200

    Initial commit, does nothing but builds empty .so

---
+++ src/lgi.c
@@ -0,0 +1,11 @@
+/*
+ * Dynamic Lua binding to GObject using dynamic GObject-Interospection.
+ *
+ * Author: Pavel Holejsovsky (pavel.holejsovsky@gmail.com)
+ * License: The same license as Lua 5.1 implementation.
+ */
+
+#include <lua.h>
+#include <lauxlib.h>
+
+#include <girepository.h>
