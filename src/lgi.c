832591c6d7cc4d2525642304d19243d334dd27a8
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-07-22 15:00:25 +0200

    Adding handling of output params, fix in gc, added tostring.

--- src/lgi.c
+++ src/lgi.c
@@ -58,7 +58,7 @@ function_new(lua_State* L, GIFunctionInfo* info)
   struct ud_function* function = lua_newuserdata(L, sizeof(struct ud_function));
   luaL_getmetatable(L, UD_FUNCTION);
   lua_setmetatable(L, -2);
-  function->info = info;
+  function->info = g_base_info_ref(info);
   if (!g_function_info_prep_invoker(info, &function->invoker, &err))
     lgi_throw(L, err);
 }
@@ -68,8 +68,18 @@ function_gc(lua_State* L)
 {
   struct ud_function* function = luaL_checkudata(L, 1, UD_FUNCTION);
   g_function_invoker_destroy(&function->invoker);
-  g_object_unref(function->info);
+  g_base_info_unref(function->info);
   return 0;
+}
+
+static int
+function_tostring(lua_State* L)
+{
+  struct ud_function* function = luaL_checkudata(L, 1, UD_FUNCTION);
+  lua_pushfstring(L, "lgifun: %s.%s %p",
+                  g_base_info_get_namespace(function->info),
+                  g_base_info_get_name(function->info), function);
+  return 1;
 }
 
 union typebox
@@ -98,20 +108,23 @@ union typebox
   gpointer gpointer_;
 };
 
+typedef void
+(*function_arg)(lua_State* L, int* argi, GITypeInfo* info,
+                GIDirection dir, union typebox*);
+
 static void
 function_arg_in(lua_State* L, int* argi, GITypeInfo* info, GIDirection dir,
-		gpointer* arg_ptr, union typebox* arg_val)
+		union typebox* arg)
 {
-  int argi_advance = 0;
-  *arg_ptr = arg_val;
+  int arg_used = 0;
   switch (g_type_info_get_tag(info))
     {
 #define TYPE_CASE(tag, type, expr)					\
       case GI_TYPE_TAG_ ## tag :					\
 	if (dir == GI_DIRECTION_IN || dir == GI_DIRECTION_INOUT)	\
 	  {								\
-	    arg_val->type ## _ = (type) expr;				\
-	    argi_advance = 1;						\
+	    arg->type ## _ = (type)expr;				\
+	    arg_used = 1;						\
 	  }								\
 	break
 
@@ -121,8 +134,8 @@ function_arg_in(lua_State* L, int* argi, GITypeInfo* info, GIDirection dir,
       TYPE_CASE(INT16, gint16, luaL_checkinteger(L, *argi));
       TYPE_CASE(UINT16, guint16, luaL_checkinteger(L, *argi));
       TYPE_CASE(INT32, guint32, luaL_checkinteger(L, *argi));
-      TYPE_CASE(INT64, gint64, luaL_checkinteger(L, *argi));
-      TYPE_CASE(UINT64, guint64, luaL_checkinteger(L, *argi));
+      TYPE_CASE(INT64, gint64, luaL_checknumber(L, *argi));
+      TYPE_CASE(UINT64, guint64, luaL_checknumber(L, *argi));
       TYPE_CASE(SHORT, gshort, luaL_checkinteger(L, *argi));
       TYPE_CASE(USHORT, gushort, luaL_checkinteger(L, *argi));
       TYPE_CASE(INT, gint, luaL_checkinteger(L, *argi));
@@ -134,19 +147,12 @@ function_arg_in(lua_State* L, int* argi, GITypeInfo* info, GIDirection dir,
       TYPE_CASE(FLOAT, gfloat, luaL_checkinteger(L, *argi));
       TYPE_CASE(DOUBLE, gdouble, luaL_checkinteger(L, *argi));
       TYPE_CASE(GTYPE, GType, luaL_checkinteger(L, *argi));
+      TYPE_CASE(UTF8, gpointer, luaL_checkstring(L, *argi));
+      TYPE_CASE(FILENAME, gpointer, luaL_checkstring(L, *argi));
 
 #undef TYPE_CASE
 
     case GI_TYPE_TAG_VOID:
-      break;
-
-    case GI_TYPE_TAG_UTF8:
-    case GI_TYPE_TAG_FILENAME:
-      if (dir == GI_DIRECTION_IN || dir == GI_DIRECTION_INOUT)
-	{
-	  arg_val->gpointer_ = (gpointer)luaL_checkstring(L, *argi);
-	  argi_advance = 1;
-	}
       break;
 
     default:
@@ -154,44 +160,77 @@ function_arg_in(lua_State* L, int* argi, GITypeInfo* info, GIDirection dir,
       break;
     }
 
-  *argi += argi_advance;
+  *argi += arg_used;
+}
+
+static void
+function_arg_out(lua_State* L, int* argi, GITypeInfo* info, GIDirection dir,
+                 union typebox* arg)
+{
+  int arg_used = 0;
+  switch (g_type_info_get_tag(info))
+    {
+#define TYPE_CASE(tag, type, push)					\
+      case GI_TYPE_TAG_ ## tag :					\
+	if (dir == GI_DIRECTION_OUT || dir == GI_DIRECTION_INOUT)	\
+	  {								\
+            push(L, arg->type ## _);                                    \
+	    arg_used = 1;						\
+	  }								\
+	break
+
+      TYPE_CASE(BOOLEAN, gboolean, lua_pushboolean);
+      TYPE_CASE(INT8, gint8, lua_pushinteger);
+      TYPE_CASE(UINT8, guint8, lua_pushinteger);
+      TYPE_CASE(INT16, gint16, lua_pushinteger);
+      TYPE_CASE(UINT16, guint16, lua_pushinteger);
+      TYPE_CASE(INT32, guint32, lua_pushinteger);
+      TYPE_CASE(INT64, gint64, lua_pushnumber);
+      TYPE_CASE(UINT64, guint64, lua_pushnumber);
+      TYPE_CASE(SHORT, gshort, lua_pushinteger);
+      TYPE_CASE(USHORT, gushort, lua_pushinteger);
+      TYPE_CASE(INT, gint, lua_pushinteger);
+      TYPE_CASE(UINT, guint, lua_pushinteger);
+      TYPE_CASE(LONG, glong, lua_pushinteger);
+      TYPE_CASE(ULONG, gulong, lua_pushinteger);
+      TYPE_CASE(SSIZE, gssize, lua_pushinteger);
+      TYPE_CASE(SIZE, gsize, lua_pushinteger);
+      TYPE_CASE(FLOAT, gfloat, lua_pushinteger);
+      TYPE_CASE(DOUBLE, gdouble, lua_pushinteger);
+      TYPE_CASE(GTYPE, GType, lua_pushinteger);
+      TYPE_CASE(UTF8, gpointer, lua_pushstring);
+      TYPE_CASE(FILENAME, gpointer, lua_pushstring);
+
+#undef TYPE_CASE
+
+    case GI_TYPE_TAG_VOID:
+      break;
+
+    default:
+      /* TODO: Handle the complex ones. */
+      break;
+    }
+
+  *argi += arg_used;
 }
 
 static int
-function_call(lua_State* L)
+function_handle_args(lua_State* L, function_arg do_arg, GICallableInfo* fi,
+                     int has_self, int throws, int argc, union typebox* args)
 {
-  gint argc, flags, has_self, throws, argi, lua_argi, ti_argi, ffi_argi;
-  gpointer* args_ptr;
-  union typebox* args_val;
-  struct ud_function* function = luaL_checkudata(L, 1, UD_FUNCTION);
-  GError* err = NULL;
+  gint argi, lua_argi = 2, ti_argi = 0, ffi_argi = 1;
   GITypeInfo* ti;
 
-  /* If function is a method, it has implicit 'self' parameter. */
-  flags = g_function_info_get_flags(function->info);
-  has_self = (flags & GI_FUNCTION_IS_METHOD) != 0 &&
-    (flags & GI_FUNCTION_IS_CONSTRUCTOR) == 0;
-  throws = (flags & GI_FUNCTION_THROWS) != 0;
-  argc = g_callable_info_get_n_args(function->info);
-
-  /* Allocate array for arguments. */
-  args_ptr = g_newa(gpointer, argc + 1 + has_self + throws);
-  args_val = g_newa(union typebox, argc + 1 + has_self + throws);
-  lua_argi = 2;
-
   /* Handle return value. */
-  ti = g_callable_info_get_return_type(function->info);
-  function_arg_in(L, &lua_argi, ti, GI_DIRECTION_OUT,
-		  &args_ptr[0], &args_val[0]);
+  ti = g_callable_info_get_return_type(fi);
+  do_arg(L, &lua_argi, ti, GI_DIRECTION_OUT, &args[0]);
   g_base_info_unref(ti);
-  ffi_argi = 1;
 
   /* Handle 'self', if the function has it. */
   if (has_self)
     {
-      ti = g_base_info_get_container(function->info);
-      function_arg_in(L, &lua_argi, ti, GI_DIRECTION_IN,
-		      &args_ptr[1], &args_val[1]);
+      ti = g_base_info_get_container(fi);
+      do_arg(L, &lua_argi, ti, GI_DIRECTION_IN, &args[1]);
       g_base_info_unref(ti);
       ffi_argi++;
     }
@@ -200,33 +239,64 @@ function_call(lua_State* L)
   ti_argi = 0;
   for (argi = 0; argi < argc; argi++)
     {
-      GIArgInfo* ai = g_callable_info_get_arg(function->info, ti_argi++);
+      GIArgInfo* ai = g_callable_info_get_arg(fi, ti_argi++);
       ti = g_arg_info_get_type(ai);
-      function_arg_in(L, &lua_argi, ti, g_arg_info_get_direction(ai),
-		      &args_ptr[ffi_argi], &args_val[ffi_argi]);
-      ffi_argi++;
+      do_arg(L, &lua_argi, ti, g_arg_info_get_direction(ai), &args[ffi_argi++]);
       g_base_info_unref(ti);
       g_base_info_unref(ai);
     }
 
+  return lua_argi - 2;
+}
+
+static int
+function_call(lua_State* L)
+{
+  gint i, argc, argffi, flags, has_self, throws;
+  gpointer* args_ptr;
+  union typebox* args_val;
+  struct ud_function* function = luaL_checkudata(L, 1, UD_FUNCTION);
+  GError* err = NULL;
+
+  /* Check general function characteristics. */
+  flags = g_function_info_get_flags(function->info);
+  has_self = (flags & GI_FUNCTION_IS_METHOD) != 0 &&
+    (flags & GI_FUNCTION_IS_CONSTRUCTOR) == 0;
+  throws = (flags & GI_FUNCTION_THROWS) != 0;
+  argc = g_callable_info_get_n_args(function->info);
+
+  /* Allocate array for arguments. */
+  argffi = argc + 1 + has_self + throws;
+  args_val = g_newa(union typebox, argffi);
+  args_ptr = g_newa(gpointer, argffi);
+  for (i = 0; i < argffi; ++i)
+    args_ptr[i] = &args_val[i];
+
+  /* Process parameters for input. */
+  function_handle_args(L, function_arg_in, function->info, has_self, throws,
+                       argc, args_val);
+
   /* Handle 'throws' parameter, if function does it. */
   if (throws)
-    args_ptr[ffi_argi++] = &err;
+    args_val[argffi - 1].gpointer_ = &err;
 
   /* Perform the call. */
   ffi_call(&function->invoker.cif, function->invoker.native_address,
 	   args_ptr[0], &args_ptr[1]);
 
-  /* Check, whether an error happened. */
-  if (throws && err != 0)
+  /* Check, whether function threw. */
+  if (err != NULL)
     return lgi_error(L, err);
 
-  return 0;
+  /* Process parameters for output. */
+  return function_handle_args(L, function_arg_out, function->info, has_self,
+                              throws, argc, args_val);
 }
 
 static const struct luaL_reg function_reg[] = {
   { "__gc", function_gc },
   { "__call", function_call },
+  { "__tostring", function_tostring },
   { NULL, NULL }
 };
 
@@ -251,7 +321,7 @@ lgi_get(lua_State* L)
   if (info == NULL)
     {
       lua_pushboolean(L, 0);
-      lua_pushfstring(L, "symbol %s.%s not found", namespace_, name_);
+      lua_pushfstring(L, "symbol %s.%s not found", namespace_, symbol);
       return 2;
     }
 
