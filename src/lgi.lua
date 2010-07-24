e256b3c22610c5843505d97617acd66c3ad9eba0
Pavel holejsovsky <pavel.holejsovsky@gmail.com>
2010-07-24 16:17:39 +0200

    Lua-side bootstrap handles enums and structs.

--- src/lgi.lua
+++ src/lgi.lua
@@ -7,7 +7,7 @@
 
 --]]--
 
-local assert, setmetatable, pairs, table = assert, setmetatable, pairs, table
+local assert, setmetatable, pairs, string = assert, setmetatable, pairs, string
 local core = require 'lgi._core'
 
 module 'lgi'
@@ -26,11 +26,21 @@ end
 local gi = {
    IRepository = getface(
       'GIRepository', 'IRepository', '', {
-	 'require', 'find_by_name', 'get_n_infos', 'get_info'
+	 'require', 'find_by_name', 'get_n_infos', 'get_info',
       }),
    IBaseInfo = getface(
       'GIRepository', nil, 'base_info_', {
-	 'unref', 'get_type', 'get_name', 'is_deprecated', 'get_container',
+	 'ref', 'unref', 'get_type', 'is_deprecated', 'get_name',
+      }),
+
+   IEnumInfo = getface(
+      'GIRepository', nil, 'enum_info_', {
+	 'get_n_values', 'get_value',
+      }),
+
+   IValueInfo = getface(
+      'GIRepository', nil, 'value_info_', {
+	 'get_value',
       }),
 
    INFO_TYPE_FUNCTION = 1,
@@ -51,13 +61,28 @@ function package_mt.__index(package, name)
 
    -- Decide according to symbol type what to do.
    local value
-   local type = gi.IBaseInfo.get_type(info)
-   if type == gi.INFO_TYPE_FUNCTION or type == gi.INFO_TYPE_CONSTANT then
-      value = core.get(info)
+   if not gi.IBaseInfo.is_deprecated(info) then
+      local type = gi.IBaseInfo.get_type(info)
+      if type == gi.INFO_TYPE_FUNCTION or type == gi.INFO_TYPE_CONSTANT then
+	 value = core.get(info)
+      elseif type == gi.INFO_TYPE_STRUCT then
+	 gi.IBaseInfo.ref(info)
+	 value = { new = function() return core.get(info) end }
+      elseif type == gi.INFO_TYPE_ENUM then
+	 value = {}
+	 for i = 0, gi.IEnumInfo.get_n_values(info) - 1 do
+	    local val = gi.IEnumInfo.get_value(info, i)
+	    local n = string.upper(gi.IBaseInfo.get_name(val))
+	    local v = gi.IValueInfo.get_value(val)
+	    value[n] = v
+	    value[v] = n
+	    gi.IBaseInfo.unref(val)
+	 end
+      end
    end
 
    gi.IBaseInfo.unref(info)
-   
+
    -- Cache the result.
    package[name] = value
    return value
