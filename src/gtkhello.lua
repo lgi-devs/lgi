c037229f5362ff216fea3e06295d5493a5be64d6
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-09-17 13:57:03 +0200

    Consolidated samples and tests.

---
+++ src/gtkhello.lua
@@ -0,0 +1,59 @@
+#! /usr/bin/env lua
+
+--
+-- Sample GTK Hello program
+--
+-- Based on test from LuiGI code.  Thanks Adrian!
+--
+
+require 'lgi'
+local Gtk = require 'lgi.Gtk'
+
+-- Initialize GTK.
+Gtk.init(0, nil)
+
+-- Create top level window with some properties and connect its 'destroy'
+-- signal to the event loop termination.
+local window = Gtk.Window {
+   title = 'window',
+   default_width = 400,
+   default_height = 300,
+   on_destroy = Gtk.main_quit
+}
+
+-- Create some more widgets for the window.
+local status_bar = Gtk.Statusbar { has_resize_grip = true }
+local toolbar = Gtk.Toolbar()
+local ctx = status_bar:get_context_id('default')
+status_bar:push(ctx, 'This is statusbar message.')
+
+-- When clicking at the toolbar 'quit' button, destroy the main window.
+toolbar:insert(Gtk.ToolButton {
+		  stock_id = 'gtk-quit',
+		  on_clicked = function() window:destroy() end,
+	       }, -1)
+
+-- About button in toolbar and its handling.
+toolbar:insert(Gtk.ToolButton {
+		  stock_id = 'gtk-about',
+		  on_clicked = function()
+				  local dlg = Gtk.AboutDialog {
+				     program_name = 'LGI Demo',
+				     title = 'About...',
+				     license = 'MIT'
+				  }
+				  dlg:run()
+				  dlg:hide()
+			       end
+	       }, -1)
+
+-- Pack everything into the window.
+local vbox = Gtk.VBox()
+vbox:pack_start(toolbar, false, false, 0)
+vbox:pack_start(Gtk.Label { label = 'Contents' }, true, true, 0)
+vbox:pack_end(status_bar, false, false, 0)
+window:add(vbox)
+
+-- Show window and start the loop.
+window:show_all()
+Gtk.main()
--- src/test.lua
+++ src/test.lua
@@ -3,9 +3,8 @@
 require 'lgi'
 local GLib = require 'lgi.GLib'
 local Gio = require 'lgi.Gio'
-local Gtk = lgi.Gtk
 
-local tests = { 'gioloadfile', 'gioloadfile_async', 'gtkhello' }
+local tests = { 'gioloadfile', 'gioloadfile_async' }
 
 function tests.gioloadfile()
    local file = Gio.file_new_for_path('test.lua')
@@ -26,50 +25,6 @@ function tests.gioloadfile_async()
    main:run()
    assert(ok and type(contents) == 'string' and type(length) == 'number' and
     type(etag) == 'string')
-end
-
-function tests.gtkhello()
-   -- Based on test from LuiGI code.  Thanks Adrian!
-   Gtk.init(0, nil)
-   local window = Gtk.Window {
-      title = 'window',
-      default_width = 400,
-      default_height = 300,
-      on_delete_event = function(window)
-			   window:destroy()
-			   Gtk.main_quit()
-			end
-   }
-   local status_bar = Gtk.Statusbar { has_resize_grip = true }
-   local toolbar = Gtk.Toolbar()
-   local vbox = Gtk.VBox()
-   local ctx = status_bar:get_context_id('default')
-   status_bar:push(ctx, 'This is statusbar message.')
-   toolbar:insert(Gtk.ToolButton {
-		     stock_id = 'gtk-quit',
-		     on_clicked = function()
-				     window:destroy()
-				     Gtk.main_quit()
-				  end,
-		  }, -1)
-   toolbar:insert(Gtk.ToolButton {
-		     stock_id = 'gtk-about',
-		     on_clicked = function()
-				     local dlg = Gtk.AboutDialog {
-					program_name = 'LGI Demo',
-					title = 'About...',
-					license = 'MIT'
-				     }
-				     dlg:run()
-				     dlg:destroy()
-				  end
-		  }, -1)
-   vbox:pack_start(toolbar, false, false, 0)
-   vbox:pack_start(Gtk.Label { label = 'Contents' }, true, true, 0)
-   vbox:pack_end(status_bar, false, false, 0)
-   window:add(vbox)
-   window:show_all()
-   Gtk.main()
 end
 
 -- Runs specified test from tests table.
