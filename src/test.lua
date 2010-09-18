3273a7f8cf2885e6e699ff5382560a6d0cf870a7
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-09-18 09:17:07 +0200

    Reorganized tests, started using GI Regress library, basic number tests.

--- src/test.lua
+++ src/test.lua
@@ -4,16 +4,142 @@ require 'lgi'
 local GLib = require 'lgi.GLib'
 local Gio = require 'lgi.Gio'
 
-local tests = { 'gio_loadfile_sync', 'gio_loadfile_async', 'gio_loadfile_coro' }
+local tests = {}
 
-function tests.gio_loadfile_sync()
+-- Helper, checks that given value has requested type and value.
+local function checkv(val, exp, exptype)
+   assert(type(val) == exptype, string.format("got type `%s', expected `%s'",
+					      type(val), exptype))
+   assert(val == exp, string.format("got value `%s', expected `%s'",
+				    tostring(val), tostring(exp)))
+end
+
+-- gobject-introspection 'Regress' based tests.
+function tests.t01_gireg_01_boolean()
+   local R = lgi.Regress
+   checkv(R.test_boolean(true), true, 'boolean')
+   checkv(R.test_boolean(false), false, 'boolean')
+end
+
+function tests.t01_gireg_02_int8()
+   local R = lgi.Regress
+   checkv(R.test_int8(0), 0, 'number')
+   checkv(R.test_int8(1), 1, 'number')
+   checkv(R.test_int8(-1), -1, 'number')
+   checkv(R.test_int8(1.1), 1, 'number')
+   checkv(R.test_int8(-1.1), -1, 'number')
+   checkv(R.test_int8(128), -128, 'number')
+   checkv(R.test_int8(255), -1, 'number')
+   checkv(R.test_int8(256), 0, 'number')
+end
+
+function tests.t01_gireg_03_uint8()
+   local R = lgi.Regress
+   checkv(R.test_uint8(0), 0, 'number')
+   checkv(R.test_uint8(1), 1, 'number')
+   checkv(R.test_uint8(-1), 255, 'number')
+   checkv(R.test_uint8(1.1), 1, 'number')
+   checkv(R.test_uint8(-1.1), 255, 'number')
+   checkv(R.test_uint8(128), 128, 'number')
+   checkv(R.test_uint8(255), 255, 'number')
+   checkv(R.test_uint8(256), 0, 'number')
+end
+
+function tests.t01_gireg_04_int16()
+   local R = lgi.Regress
+   checkv(R.test_int16(0), 0, 'number')
+   checkv(R.test_int16(1), 1, 'number')
+   checkv(R.test_int16(-1), -1, 'number')
+   checkv(R.test_int16(1.1), 1, 'number')
+   checkv(R.test_int16(-1.1), -1, 'number')
+   checkv(R.test_int16(32768), -32768, 'number')
+   checkv(R.test_int16(65535), -1, 'number')
+   checkv(R.test_int16(65536), 0, 'number')
+end
+
+function tests.t01_gireg_05_uint16()
+   local R = lgi.Regress
+   checkv(R.test_uint16(0), 0, 'number')
+   checkv(R.test_uint16(1), 1, 'number')
+   checkv(R.test_uint16(-1), 65535, 'number')
+   checkv(R.test_uint16(1.1), 1, 'number')
+   checkv(R.test_uint16(-1.1), 65535, 'number')
+   checkv(R.test_uint16(32768), 32768, 'number')
+   checkv(R.test_uint16(65535), 65535, 'number')
+   checkv(R.test_uint16(65536), 0, 'number')
+end
+
+function tests.t01_gireg_06_int32()
+   local R = lgi.Regress
+   checkv(R.test_int32(0), 0, 'number')
+   checkv(R.test_int32(1), 1, 'number')
+   checkv(R.test_int32(-1), -1, 'number')
+   checkv(R.test_int32(1.1), 1, 'number')
+   checkv(R.test_int32(-1.1), -1, 'number')
+   checkv(R.test_int32(0x80000000), -0x80000000, 'number')
+   checkv(R.test_int32(0xffffffff), -1, 'number')
+   checkv(R.test_int32(0x100000000), 0, 'number')
+end
+
+function tests.t01_gireg_07_uint32()
+   local R = lgi.Regress
+   checkv(R.test_uint32(0), 0, 'number')
+   checkv(R.test_uint32(1), 1, 'number')
+   checkv(R.test_uint32(-1), 0xffffffff, 'number')
+   checkv(R.test_uint32(1.1), 1, 'number')
+   checkv(R.test_uint32(-1.1), 0xffffffff, 'number')
+   checkv(R.test_uint32(0x80000000), 0x80000000, 'number')
+   checkv(R.test_uint32(0xffffffff), 0xffffffff, 'number')
+   checkv(R.test_uint32(0x100000000), 0, 'number')
+end
+
+function tests.t01_gireg_08_int64()
+   local R = lgi.Regress
+   checkv(R.test_int64(0), 0, 'number')
+   checkv(R.test_int64(1), 1, 'number')
+   checkv(R.test_int64(-1), -1, 'number')
+   checkv(R.test_int64(1.1), 1, 'number')
+   checkv(R.test_int64(-1.1), -1, 'number')
+
+   checkv(R.test_int64(0x80000000), 0x80000000, 'number')
+   checkv(R.test_int64(0xffffffff), 0xffffffff, 'number')
+   checkv(R.test_int64(0x100000000), 0x100000000, 'number')
+
+-- Following tests fail because Lua's internal number representation
+-- is always 'double', and conversion between double and int64 big
+-- constants is always lossy.  Not sure if it can be solved somehow.
+
+-- checkv(R.test_int64(0x8000000000000000), -0x8000000000000000, 'number')
+-- checkv(R.test_int64(0xffffffffffffffff), -1, 'number')
+-- checkv(R.test_int64(0x10000000000000000), 0, 'number')
+end
+
+function tests.t01_gireg_09_uint64()
+   local R = lgi.Regress
+   checkv(R.test_uint64(0), 0, 'number')
+   checkv(R.test_uint64(1), 1, 'number')
+   checkv(R.test_uint64(-1), 0xffffffffffffffff, 'number')
+   checkv(R.test_uint64(1.1), 1, 'number')
+   checkv(R.test_uint64(-1.1), 0xffffffffffffffff, 'number')
+
+   checkv(R.test_int64(0x80000000), 0x80000000, 'number')
+   checkv(R.test_int64(0xffffffff), 0xffffffff, 'number')
+   checkv(R.test_int64(0x100000000), 0x100000000, 'number')
+   checkv(R.test_uint64(0x8000000000000000), 0x8000000000000000, 'number')
+   checkv(R.test_uint64(0x10000000000000000), 0, 'number')
+
+-- See comment above about lossy conversions.
+-- checkv(R.test_uint64(0xffffffffffffffff), 0xffffffffffffffff, 'number')
+end
+
+function tests.t02_gio_01_loadfile_sync()
    local file = Gio.file_new_for_path('test.lua')
    local ok, contents, length, etag = file:load_contents()
    assert(ok and type(contents) == 'string' and type(length) == 'number' and
     type(etag) == 'string')
 end
 
-function tests.gio_loadfile_async()
+function tests.t02_gio_02_loadfile_async()
    local file = Gio.file_new_for_path('test.lua')
    local ok, contents, length, etag
    local main = GLib.MainLoop.new()
@@ -27,7 +153,7 @@ function tests.gio_loadfile_async()
     type(etag) == 'string')
 end
 
-function tests.gio_loadfile_coro()
+function tests.t02_gio_03_loadfile_coro()
    local ok, contents, length, etag
    local main = GLib.MainLoop.new()
    coroutine.wrap(
@@ -46,6 +172,9 @@ function tests.gio_loadfile_coro()
     type(etag) == 'string')
 end
 
+local tests_passed = 0
+local tests_failed = 0
+
 -- Runs specified test from tests table.
 local function runtest(name)
    local func = tests[name]
@@ -55,13 +184,28 @@ local function runtest(name)
       local ok, msg = pcall(tests[name])
       if ok then
 	 print(string.format('PASS: %s', name))
+	 tests_passed = tests_passed + 1
       else
 	 print(string.format('FAIL: %s: %s', name, tostring(msg)))
+	 tests_failed = tests_failed + 1
       end
    end
+end
+
+do
+   local names = {}
+   for name in pairs(tests) do names[#names + 1] = name end
+   table.sort(names)
+   for i, name in ipairs(names) do tests[i] = name end
 end
 
 -- Run all tests from commandline, or all tests sequentially, if not
 -- commandline is given.
 local args = {...}
 for _, name in ipairs(#args > 0 and args or tests) do runtest(name) end
+local tests_total = tests_failed + tests_passed
+if tests_failed == 0 then
+   print(string.format('All %d tests passed.', tests_total))
+else
+   print(string.format('%d of %d tests FAILED!', tests_failed, tests_total))
+end
