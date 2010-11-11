9941d1e5f57c6155eabaccb6dc2434e0bc50c8d7
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-11-11 17:02:12 +0100

    Initial attempt at gstreamer sample (Vala conversion).

---
+++ samples/gst-play-stream.lua
@@ -0,0 +1,38 @@
+#! /usr/bin/env lua
+
+--
+-- Sample GStreamer application, port of public Vala GStreamer Audio
+-- Stream Example (http://live.gnome.org/Vala/GStreamerSample)
+--
+
+require 'lgi'
+local GLib = require 'lgi.GLib'
+local Gtk = require 'lgi.Gtk'
+local Gst = require 'lgi.Gst'
+
+Gtk.init()
+Gst.init()
+
+local function bus_callback(bus, message)
+   if message.type == Gst.MessageType.ERROR then
+      print 'error'
+      Gtk.main_quit()
+   elseif message.type == Gst.MessageType.EOS then
+      print 'end of stream'
+   elseif message.type == Gst.MessageType.STATE_CHANGED then
+      local old, new, pending = message.parse_state_changed()
+      print(('state changed: %s->%s:%s'):format(
+	       Gst.State[old], Gst.State[new], Gst.State[pending]))
+   elseif message.type == Gst.MessageType.TAG then
+      print('taglist found')
+   end
+
+   return true
+end
+
+local play = Gst.ElementFactory.make('playbin', 'play')
+play.uri = 'http://streamer-dtc-aa02.somafm.com:80/stream/1018'
+play.bus:add_watch_full(GLib.PRIORITY_DEFAULT, bus_callback)
+
+-- Run the loop.
+Gtk.main()
