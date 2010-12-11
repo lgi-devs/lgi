6ff98754b73ef9a5740dee7da3cf0f507e86e405
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-12-11 06:31:25 +0100

    Add Gtk override, for now only automatically initializes Gtk.

---
+++ src/lgix-Gtk-3_0.lua
@@ -0,0 +1,15 @@
+------------------------------------------------------------------------------
+--
+--  LGI Gtk3 override module.
+--
+--  Copyright (c) 2010 Pavel Holejsovsky
+--  Licensed under the MIT license:
+--  http://www.opensource.org/licenses/mit-license.php
+--
+------------------------------------------------------------------------------
+
+local lgi = require 'lgi'
+local Gtk = lgi.Gtk
+
+-- Initialize GTK.
+Gtk.init()
