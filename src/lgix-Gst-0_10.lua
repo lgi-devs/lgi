281ec9375928bd8c79208d08f3da530fbc85fc05
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2010-12-11 06:30:24 +0100

    Add some Gst helper overrides.

--- src/lgix-Gst-0_10.lua
+++ src/lgix-Gst-0_10.lua
@@ -10,11 +10,24 @@
 
 local lgi = require 'lgi'
 local gi = require('lgi._core').gi
+local GLib = lgi.GLib
 local Gst = lgi.Gst
 
 -- GstObject has special ref_sink mechanism, make sure that lgi core
 -- is aware of it, otherwise refcounting is screwed.
 Gst.Object._sink = gi.Gst.Object.methods.ref_sink
 
+function Gst.Bus:add_watch(callback)
+   return self:add_watch_full(GLib.PRIORITY_DEFAULT, callback)
+end
+
+function Gst.Bin:add_many(...)
+   local args = {...}
+   for i = 1, #args do self:add(args[i]) end
+end
+
 -- Load additional Gst modules.
 local GstInterfaces = lgi.GstInterfaces
+
+-- Initialize gstreamer.
+Gst.init()
