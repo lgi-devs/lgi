6dcce8614349b6bc38b3b936d775207791fa2c7d
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-01-22 14:41:12 +0100

    Add more Gtk overrides, WIP

--- src/lgix-Gtk.lua
+++ src/lgix-Gtk.lua
@@ -8,11 +8,78 @@
 --
 ------------------------------------------------------------------------------
 
-local select, type = select, type
+local select, type, pairs = select, type, pairs
 local lgi = require 'lgi'
+local core = require 'lgi._core'
 local Gtk = lgi.Gtk
+local GObject = lgi.GObject
 
 -------------------- Gtk.Container
+function Gtk.Container:child_set(child, properties)
+   -- Assign a bunch (table) of child properties at once.
+   Gtk.Widget.freeze_child_notify(child)
+   for name, value in pairs(properties) do
+      -- Ignore non-string names.
+      if type(name) == 'string' then
+	 Gtk.Container.child_set_property(self, child, 
+					  name:gsub('_', '%-'), value);
+      end
+   end
+   Gtk.Widget.thaw_child_notify(child)
+end
+
+function Gtk.Container:add_with_properties(child, properties)
+   Gtk.Container.add(self, child)
+   Gtk.Container.child_set(self, child, properties)
+end
+
+local container_prop_child_mt = {}
+function container_prop_child_mt:__index(name)
+   if name == 'on_notify' then
+      -- TODO: Handling of child_notify signal.
+   else
+      local class = core.object.query(instance, 'class', Gtk.Container)
+      local pspec = class:find_child_property(name:gsub('_', '%-'))
+      local value = GObject.Value(pspec.value_type)
+      class.get_child_property(self._container, self._child, pspec.param_id,
+			       value, pspec)
+      return value.data
+   end
+end
+
+function container_prop_child_mt:__newindex(name, newval)
+   if name == 'on_notify' then
+      -- TODO: Handling of child_notify signal.
+   else
+      -- Set specific child property.
+      local class = core.object.query(instance, 'class', Gtk.Container)
+      local pspec = class:find_child_property(name:gsub('_', '%-'))
+      local value = GObject.Value(pspec.value_type, newval)
+      class.set_child_property(self._container, self._child, pspec.param_id,
+			       value, pspec)
+   end
+end
+
+local container_prop_children_mt = {}
+function container_prop_children_mt:__index(child)
+   -- Return table which retrieves child properties on-demand.
+   return setmetatable({ _container = self._container, _child = child },
+		       container_prop_child_mt)
+end
+
+function container_prop_children_mt:__newindex(child, properties)
+   -- Proxies to child_set().
+   Gtk.Container.child_set(self._container, child, properties)
+end
+
+local function container_add_child(container, child_specs)
+   if type(child_specs) == 'table' then
+      Gtk.Container.add_with_properties(container, child_specs[1], child_specs)
+   else
+      Gtk.Container.add(container, child_specs)
+   end
+end
+
 local container_access_element = Gtk.Container._access_element
 function Gtk.Container:_access_element(container, name, element, ...)
    if name == 'children' then
@@ -20,17 +87,46 @@ function Gtk.Container:_access_element(container, name, element, ...)
 	 -- Reading yields the table of all children.
 	 return self.get_children(container)
       else
-	 -- Writing adds new children.
+	 -- Writing adds new children, optionally with specified child
+	 -- properties.
 	 local arg = ...
-	 for i = 1, #arg do self.add(container, arg[i]) end
+	 for i = 1, #arg do container_add_child(container, arg[i]) end
 	 return
+      end
+   elseif name == 'child' then
+      if select('#', ...) == 0 then
+	 -- Reading generates table used for getting/setting child
+	 -- properties on specified child.
+	 return setmetatable({ _container = container },
+			     container_prop_children_mt)
+      else
+	 -- Writing adds new child.
+	 container_add_child(container, ...)
       end
    end
 
    return container_access_element(self, container, name, element, ...)
 end
 
+-------------------- Gtk.CellLayout
+
+local celllayout_access_element = Gtk.CellLayout._access_element
+function Gtk.CellLayout:_access_element(layout, name, element, ...)
+   if name == 'attributes' then
+      assert(select('#', ...) == 1, "Gtk.CellLayout: '" .. name .. 
+	  "' is not readable")
+      self.clear(column)
+      for attr, column_number in pairs(...) do
+	 if type(attr) == 'userdata' then attr = attr.name end
+	 self.add_attribute(column, core.object.env(column).cell, attr,
+			    column_number)
+      end
+   end
+   return celllaout_access_element(layout, name, element, ...)
+end
+
 -------------------- Gtk.Builder
+
 local builder_objects_mt = {}
 function builder_objects_mt:__index(name)
    if type(name) == 'string' then
