45360af60a5c03f082a70844ccd7abbdad174573
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-02-24 12:27:26 +0100

    Improve clutter demo

--- samples/clutterdemo.lua
+++ samples/clutterdemo.lua
@@ -15,31 +15,48 @@ local stage = Clutter.Stage.get_default()
 stage.color = Clutter.Color { alpha = 255 }
 stage.width = 512
 stage.height = 512
+stage.title = 'LGI Clutter Demo'
 
 local rects = {}
 for i = 1, 6 do
    rects[i] = Clutter.Rectangle {
       color = Clutter.Color {
-	 red = 256 / 6 * (i - 1),
-	 green = 256 / 6 * (6 - i),
-	 blue = 256 / 12 * (i - 1),
+	 red = 256 / 6 * ((i - 1) % 6),
+	 green = 256 / 6 * ((i + 3) % 6),
+	 blue = 256 / 6 * ((-i + 8) % 6),
 	 alpha = 128
       },
       width = 100, height = 100,
-      fixed_x = 100, fixed_y = 100,
+      fixed_x = 200, fixed_y = 200,
+      anchor_x = 128, anchor_y = 64,
+      reactive = true,
+      on_button_press_event = function(rect) rect:raise_top() return true end,
    }
    stage:add_actor(rects[i])
    rects[i]:show()
 end
 
 local timeline = Clutter.Timeline { duration = 60, loop = true }
-local rotation = 0
+local rotation, rotation_delta = 0, 0.01
+local scale, scale_delta = 1, 0.001
 function timeline:on_new_frame(frame_num)
-   rotation = rotation + 0.3
+   rotation = rotation + rotation_delta
+   scale = scale + scale_delta
+   if scale > 2 or scale < 1 then scale_delta = -scale_delta end
    for i = 1, #rects do
       rects[i]:set_rotation(Clutter.RotateAxis.Z_AXIS, rotation * (#rects - i),
 			    0, 0, 0)
+      rects[i]:set_scale(scale, 3 - scale)
    end
+
+   -- A bug in clutter?  If following line is not present, stage stops
+   -- redrawing itself after a while...
+   stage:queue_redraw()
+end
+
+function stage:on_button_press_event(event)
+   app:release()
+   return true
 end
 
 function app:on_activate()
