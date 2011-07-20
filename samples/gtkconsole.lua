25e8f4f3772e96c15c29c900c9f578dde11794fa
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-07-20 17:21:09 +0200

    Make gtkconsole work as a very stupid Lua console

--- samples/gtkconsole.lua
+++ samples/gtkconsole.lua
@@ -13,13 +13,32 @@ local Vte = lgi.Vte
 local app = Gtk.Application { application_id = 'org.lgi.samples.gtkconsole' }
 
 -- Create terminal widget.
-local terminal = Vte.Terminal {}
+local terminal = Vte.Terminal {
+   input = '',
+}
 
 -- Invoked when something is typed into the terminal.
 function terminal:on_commit(text, length)
    if text == '\r' then
+      -- Jump to the next line.
       self:feed('\27[E', 3)
+
+      -- Try to execute input line.
+      local chunk, msg = loadstring(self.input)
+      if not chunk then
+	 answer = msg
+      else
+	 local ok, res = pcall(chunk)
+	 answer = tostring(res)
+      end
+      self:feed(answer, #answer)
+      self:feed('\27[E', 3)
+
+      -- Prepare empty input line for the next statement.
+      self.input = ''
    else
+      -- Simply echo to the terminal and add to the inputline.
+      self.input = self.input .. text
       self:feed(text, length)
    end
 end
