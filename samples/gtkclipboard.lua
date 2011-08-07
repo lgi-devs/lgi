7a2ac2ddcc16df901fad3b9239822bad5471f62b
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-08-07 16:50:44 +0200

    Add simple port of Gtk clipboard Vala sample

---
+++ samples/gtkclipboard.lua
@@ -0,0 +1,35 @@
+#! /usr/bin/env lua
+
+-- Gtk clipboard sample, adapted from Vala clipboard sample at
+-- http://live.gnome.org/Vala/GTKSample#Clipboard
+
+local lgi = require 'lgi'
+local Gtk = lgi.Gtk
+local Gdk = lgi.Gdk
+
+local app = Gtk.Application { application_id = 'org.lgi.samples.gtkclipboard' }
+
+function app:on_activate()
+   local entry = Gtk.Entry {}
+   local window = Gtk.Window {
+      application = self,
+      title = 'Clipboard',
+      default_width = 300, default_height = 20,
+      child = entry
+   }
+   window:show_all()
+
+   local display = window:get_display()
+   local clipboard = Gtk.Clipboard.get_for_display(
+      display, Gdk.SELECTION_CLIPBOARD)
+
+   -- Get text from clipboard
+   entry.text = clipboard:wait_for_text() or ''
+
+   -- If the user types something, set text to clipboard
+   function entry:on_changed()
+      clipboard:set_text(entry.text, -1)
+   end
+end
+
+app:run { arg[0], ... }
