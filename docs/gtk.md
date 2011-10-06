1da86080bbec7355267c52239ccde7a561dd72d3
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-10-06 21:25:32 +0200

    Add basic Gtk overrides
    Implementation of style properties and child properties in overrides.
    Also add some documentation for them.

--- docs/gtk.md
+++ docs/gtk.md
@@ -1,33 +1,37 @@
-## Gtk
+# Gtk support
 
-### Container
+LGI Gtk support is based on base gobject-introspection support.  Some
+extensions are provided to support non-introspectable features and to
+provide easier and more Lua-like access to some Gtk features.
 
-Properties:
+## Basic Widget and Container support
 
-- `children`
+### Style properties access
 
-### Builder
+To read style property values of the widget, a `style` attribute is
+implemented.  Following example reads `resize-grip-height` style
+property from Gtk.Window instance:
 
-Properties:
+    local window = Gtk.Window()
+    print(window.style.resize_grip_height)
+    
+### Child properties
 
-- `file`
+Child properties are properties of the relation between a container
+and child.  A Lua-friendly access to these properties is implemented
+by `children` attribute of `Gtk.Container`.  Following example
+illustrates writing and reading of `x-padding` property of `Gtk.Table`
+and child `Gtk.Button`:
 
-     write: string: adds file to builder instance using
-     `add_from_file()`, throws when adding fails.
+    local table, button = Gtk.Table(), Gtk.Button()
+    table:add(button)
+    table.children[button].x_padding = 10
+    print(table.children[button].x_padding)   -- prints 10
 
-     write: table(array,string):  adds files to builder instance using
-     `add_from_file()`, throws when adding fails.
+`Gtk.Container:add()` is overloaded so that it accepts optional second
+argument containing table with child property/value pairs.  Previous
+example can be rewritten like this:
 
-- `string`
-
-     write: string: adds string description to builder instance using
-     `add_from_string()`, throws when adding fails.
-
-     write: table(array,string): adds string descriptions to builder
-     instance using `add_from_string()`, throws when adding fails.
-
-- `objects` read-only, table
-
-    Returns table containing loaded objects; array part contains all
-    objects, accessing table by named index on-demand loads object
-    with given id.PP
+    local table, button = Gtk.Table(), Gtk.Button()
+    table:add(button, { x_padding = 10 })
+    print(table.children[button].x_padding)   -- prints 10
--- src/lgix/Gtk.lua
+++ src/lgix/Gtk.lua
@@ -11,14 +11,76 @@
 local select, type, pairs, unpack = select, type, pairs, unpack
 local lgi = require 'lgi'
 local core = require 'lgi._core'
+local gi = core.gi
 local Gtk = lgi.Gtk
+local Gdk = lgi.Gdk
 local GObject = lgi.GObject
 
+-- Gtk.Allocation is just an alias to Gdk.Rectangle.
+Gtk.Allocation = Gdk.Rectangle
 
+-------------------------------- Gtk.Widget overrides.
+Gtk.Widget._attribute = {}
 
+-- gtk_widget_intersect is missing an (out caller-allocates) annotation
+if core.gi.Gtk.Widget.methods.intersect.args[2].direction == 'in' then
+   local real_intersect = Gtk.Widget.intersect
+   function Gtk.Widget._method:intersect(area)
+      local intersection = Gdk.Rectangle()
+      local notempty = real_intersect(self, area, intersection)
+      return notempty and intersection or nil
    end
 end
 
+-- Accessing style properties is preferrably done by accessing 'style'
+-- property.  In case that caller wants deprecated 'style' property, it
+-- must be accessed by '_property_style' name.
+Gtk.Widget._attribute.style = {}
+local style_mt = {}
+function style_mt:__index(name)
+   name = name:gsub('_', '%-')
+   local pspec = self._widget.class:find_style_property(name)
+   local value = GObject.Value(pspec.value_type)
+   self._widget:style_get_property(name, value)
+   return value.value
+end
+function Gtk.Widget._attribute.style:get()
+   return setmetatable({ _widget = self }, style_mt)
+end
+
+-------------------------------- Gtk.Container overrides.
+Gtk.Container._attribute = {}
+
+-- Accessing child properties is preferrably done by accessing
+-- 'children' property.
+Gtk.Container._attribute.children = {}
+local child_mt = {}
+function child_mt:__index(name)
+   name = name:gsub('_', '%-')
+   local pspec = self._container.class:find_child_property(name)
+   local value = GObject.Value(pspec.value_type)
+   self._container:child_get_property(self._child, name, value)
+   return value.value
+end
+function child_mt:__newindex(name, val)
+   name = name:gsub('_', '%-')
+   local pspec = self._container.class:find_child_property(name)
+   local value = GObject.Value(pspec.value_type, val)
+   self._container:child_set_property(self._child, name, value)
+end
+local children_mt = {}
+function children_mt:__index(child)
+   return setmetatable({ _container = self._container, _child = child }, child_mt)
+end
+function Gtk.Container._attribute.children:get()
+   return setmetatable({ _container = self }, children_mt)
+end
+
+local container_add = Gtk.Container.add
+function Gtk.Container._method:add(widget, props)
+   container_add(self, widget)
+   local child_props = self.children[widget]
+   for name, value in pairs(props or {}) do child_props[name] = value end
 end
 
 -- Initialize GTK.
