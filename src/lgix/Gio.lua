e160d8864e90804a343a2f50237a3cbdb54f54bf
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-10-06 21:22:10 +0200

    Remove Gtk and Gio overrides
    These overrides worked, but introduced too big differnces from the
    official API.  New features departing from original upstream APIs
    should be more scare and better thought-out.

--- src/lgix/Gio.lua
+++ src/lgix/Gio.lua
@@ -13,34 +13,3 @@ local lgi = require 'lgi'
 local core = require 'lgi._core'
 local Gio = lgi.Gio
 local GObject = lgi.GObject
-
--------------------- Gio.File
-Gio.File._accessor = { contents = {} }
-function Gio.File._accessor.contents.read(file)
-   local ok, contents = assert(Gio.File.load_contents(file))
-   return contents
-end
-
--- Move new_for_xxx into Gio.File scope.
-for _, name in ipairs { 'path', 'uri', 'commandline_arg' } do
-   if not Gio.File._method['new_for_' .. name] then
-      Gio.File._method['new_for_' .. name] = Gio['file_new_for_' .. name]
-      Gio['file_new_for_' .. name] = nil
-   end
-end
-Gio.File._method.new_for_parse_name = Gio.file_parse_name
-Gio.file_parse_name = nil
-
-local file_parse_name = Gio.File._method.parse_name
-Gio.File._method.parse_name = nil
-function Gio.File.new_for_parse_name(name)
-   return file_parse_name(name)
-end
-
--- Add accessor properties for filename's get_foo methods.
-for _, name in pairs { 'basename', 'path', 'uri', 'parse_name' }  do
-   Gio.File._accessor[name] = {
-      get = function(file)
-		return Gio.File['get_' .. name](file)
-	     end }
-end
