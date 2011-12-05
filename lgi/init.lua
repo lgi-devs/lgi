c5a7c2ac2cd5e7cef383f7f1e05a2a1ad26abc7b
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-12-05 20:50:43 +0100

    Split record_mt out of init.lua

--- lgi/init.lua
+++ lgi/init.lua
@@ -56,86 +56,7 @@ log.message('gobject-introspection binding for Lua, ' .. lgi._VERSION)
 local repo = core.repo
 
 local component = require 'lgi.component'
-
--- Implementation of record_mt, which is inherited from component
--- and provides customizations for structures and unions.
-local record_mt = component.mt:clone { '_method', '_field' }
-
-function record_mt:_element(instance, symbol)
-   -- First of all, try normal inherited functionality.
-   local element, category = component.mt._element(self, instance, symbol)
-   if element then return element, category end
-
-   -- If the record has parent struct, try it there.
-   local parent = rawget(self, '_parent')
-   if parent then
-      element, category = parent:_element(instance, symbol)
-      if element then
-	 -- If category shows that returned element is already from
-	 -- inherited, leave it so, otherwise wrap returned element
-	 -- into _inherited category.
-	 if category ~= '_inherited' then
-	    element = { element = element, category = category,
-			symbol = symbol, type = parent }
-	    category = '_inherited'
-	 end
-	 return element, category
-      end
-   end
-end
-
--- Add accessor for handling fields.
-function record_mt:_access_field(instance, element, ...)
-   assert(gi.isinfo(element) and element.is_field)
-   -- Check the type of the field.
-   local ii = element.typeinfo.interface
-   if ii and (ii.type == 'struct' or ii.type == 'union') then
-      -- Nested structure, handle assignment to it specially.  Get
-      -- access to underlying nested structure.
-      local subrecord = core.record.field(instance, element)
-
-      -- Reading it is simple, we are done.
-      if select('#', ...) == 0 then return subrecord end
-
-      -- Writing means assigning all fields from the source table.
-      for name, value in pairs(...) do subrecord[name] = value end
-   else
-      -- In other cases, just access the instance using given info.
-      return core.record.field(instance, element, ...)
-   end
-end
-
--- Add accessor for accessing inherited elements.
-function record_mt:_access_inherited(instance, element, ...)
-   -- Cast instance to inherited type.
-   instance = core.record.cast(instance, element.type)
-
-   -- Forward to normal _access_element implementation.
-   return self:_access_element(instance, element.category, element.symbol,
-			       element.element, ...)
-end
-
--- Create structure instance and initialize it with given fields.
-function record_mt:_new(fields)
-   -- Find baseinfo of requested record.
-   local info
-   if self._gtype then
-      -- Try to lookup info by gtype.
-      info = gi[self._gtype]
-   end
-   if not info then
-      -- GType is not available, so lookup info by name.
-      local ns, name = self._name:match('^(.-)%.(.+)$')
-      info = assert(gi[ns][name])
-   end
-
-   -- Create the structure instance.
-   local struct = core.record.new(info)
-
-   -- Set values of fields.
-   for name, value in pairs(fields or {}) do struct[name] = value end
-   return struct
-end
+local record = require 'lgi.record'
 
 -- Implementation of interface_mt.
 local interface_mt = component.mt:clone {
@@ -327,39 +248,15 @@ local function load_method(mi)
    end
 end
 
--- Loads structure information into table representing the structure
-local function load_record(info)
-   local record = create_component(info, record_mt)
-   record._method = component.get_category(info.methods, core.callable.new)
-   record._field = component.get_category(info.fields)
-
-   -- Check, whether global namespace contains 'constructor' method,
-   -- i.e. method which has the same name as our record type (except
-   -- that type is in CamelCase, while method is
-   -- under_score_delimited).  If not found, check for 'new' method.
-   local func = info.name:gsub('([%l%d])([%u])', '%1_%2'):lower()
-   local ctor = gi[info.namespace][func]
-   if not ctor then ctor = info.methods.new end
-
-   -- Check, whether ctor is valid.  In order to be valid, it must
-   -- return instance of this record.
-   if (ctor and ctor.return_type.tag =='interface'
-       and ctor.return_type.interface == info) then
-      ctor = core.callable.new(ctor)
-      record._new = function(typetable, ...) return ctor(...) end
-   end
-   return record
-end
-
 function typeloader.struct(namespace, info)
    -- Avoid exposing internal structs created for object implementations.
    if not info.is_gtype_struct then
-      return load_record(info), '_struct'
+      return record.load(info), '_struct'
    end
 end
 
 function typeloader.union(namespace, info)
-   return load_record(info), '_union'
+   return record.load(info), '_union'
 end
 
 local function load_properties(info)
@@ -397,7 +294,7 @@ function typeloader.interface(namespace, info)
    if type_struct then
       interface._virtual = component.get_category(
 	 info.vfuncs, nil, load_vfunc_name, load_vfunc_name_reverse)
-      interface._class = load_record(type_struct)
+      interface._class = record.load(type_struct)
    end
    interface._new = find_constructor(info)
    return interface, '_interface'
@@ -428,7 +325,7 @@ function typeloader.object(namespace, info)
    if type_struct then
       class._virtual = component.get_category(
 	 info.vfuncs, nil, load_vfunc_name, load_vfunc_name_reverse)
-      class._class = load_record(type_struct)
+      class._class = record.load(type_struct)
       if parent then class._class._parent = parent._class end
    end
 
