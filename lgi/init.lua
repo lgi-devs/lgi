ee7065c3380bbe1b42202710d2ac78ae56fa0c7e
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2011-12-05 20:32:10 +0100

    Split component_mt out of init.lua

--- lgi/init.lua
+++ lgi/init.lua
@@ -55,257 +55,15 @@ log.message('gobject-introspection binding for Lua, ' .. lgi._VERSION)
 -- loading on-demand.  Created by C-side bootstrap.
 local repo = core.repo
 
--- Gets table for category of compound (i.e. _field of struct or _property
--- for class etc).  Installs metatable which performs on-demand lookup of
--- symbols.
-local function get_category(children, xform_value,
-			    xform_name, xform_name_reverse)
-   -- Either none or both transform methods must be provided.
-   assert(not xform_name or xform_name_reverse)
+local component = require 'lgi.component'
 
-   -- Early shortcircuit; no elements, no table needed at all.
-   if #children == 0 then return nil end
-
-   -- Index contains array of indices which were still not retrieved
-   -- from 'children' table, and table part contains name->index
-   -- mapping.
-   local index, mt = {}, {}
-   for i = 1, #children do index[i] = i end
-
-   -- Fully resolves the category (i.e. loads everything remaining to
-   -- be loaded in given category) and disconnects on-demand loading
-   -- metatable.
-   local function resolve(category)
-      -- Load al values from unknown indices.
-      local ei, en, val
-      local function xvalue(arg)
-	 if not xform_value then return arg end
-	 if arg then
-	    local ok, res = pcall(xform_value, arg)
-	    return ok and res
-	 end
-      end
-      while #index > 0 do
-	 ei = children[table.remove(index)]
-	 val = xvalue(ei)
-	 if val then
-	    en = ei.name
-	    en = not xform_name_reverse and en or xform_name_reverse(en)
-	    if en then category[en] = val end
-	 end
-      end
-
-      -- Load all known indices.
-      for en, idx in pairs(index) do
-	 val = xvalue(children[idx])
-	 en = not xform_name_reverse and en or xform_name_reverse(en)
-	 if en then category[en] = val end
-      end
-
-      -- Metatable is no longer needed, disconnect it.
-      return setmetatable(category, nil)
-   end
-
-   function mt:__index(requested_name)
-      -- Check if closure for fully resolving the category is needed.
-      if requested_name == '_resolve' then return resolve end
-
-      -- Transform name by transform function.
-      local name = not xform_name and requested_name
-	 or xform_name(requested_name)
-      if not name then return end
-
-      -- Check, whether we already know its index.
-      local idx, val = index[name]
-      if idx then
-	 -- We know at least the index, so get info directly.
-	 val = children[idx]
-	 index[name] = nil
-      else
-	 -- Not yet, go through unknown indices and try to find the
-	 -- name.
-	 while #index > 0 do
-	    idx = table.remove(index)
-	    val = children[idx]
-	    local en = val.name
-	    if en == name then break end
-	    val = nil
-	    index[en] = idx
-	 end
-      end
-
-      -- If there is nothing in the index, we can disconnect
-      -- metatable, because everything is already loaded.
-      if not next(index) then
-	 setmetatable(self, nil)
-      end
-
-      -- Transform found value and store it into the category (self)
-      -- table.
-      if not val then return nil end
-      if xform_value then val = xform_value(val) end
-      if not val then return nil end
-      self[requested_name] = val
-      return val
-   end
-   return setmetatable({}, mt)
-end
-
--- Generic component metatable.  Component is any entity in the repo,
--- e.g. record, object, enum, etc.
-local component_mt = {}
-
--- Creates new component table by cloning all contents and setting
--- categories table.
-function component_mt:clone(categories)
-   local new_component = {}
-   for key, value in pairs(self) do new_component[key] = value end
-   if categories then
-      table.insert(categories, 1, '_attribute')
-      new_component._categories = categories
-   end
-   return new_component
-end
-
--- __index implementation, uses _element method to perform lookup.
-function component_mt:__index(key)
-   -- First try to invoke our own _element method.
-   local _element, mt = rawget(self, '_element')
-   if not _element then
-      mt = getmetatable(self)
-      _element = rawget(mt, '_element')
-   end
-   local value = _element(self, nil, key)
-   if value then return value end
-
-   -- If not found as object element, examine the metatable itself.
-   return rawget(mt or getmetatable(self), key)
-end
-
--- __call implementation, uses _new method to create new instance of
--- component type.
-function component_mt:__call(...)
-   return self:_new(...)
-end
-
--- Fully resolves the whole typetable, i.e. load all symbols normally
--- loaded on-demand at once.  Returns self, so that resolve can be
--- easily chained for the caller.
-function component_mt:_resolve()
-   local categories = self._categories or {}
-   for i = 1, #categories do
-      -- Invoke '_resolve' function for all category tables, if they have it.
-      local category = rawget(self, categories[i])
-      local resolve = type(category) == 'table' and category._resolve
-      if resolve then resolve(category) end
-   end
-   return self
-end
-
--- Implementation of _access method, which is called by _core when
--- repo instance is accessed for reading or writing.
-function component_mt:_access(instance, symbol, ...)
-   -- Invoke _element, which converts symbol to element and category.
-   local element, category = self:_element(instance, symbol)
-   if not element then
-      error(("%s: no `%s'"):format(self._name, symbol), 3)
-   end
-   return self:_access_element(instance, category, symbol, element, ...)
-end
-
--- Internal worker of access, which works over already resolved element.
-function component_mt:_access_element(instance, category, symbol, element, ...)
-   -- Get category handler to be used, and invoke it.
-   if category then
-      local handler = self['_access' .. category]
-      if handler then return handler(self, instance, element, ...) end
-   end
-
-   -- If specific accessor does not exist, consider the element to be
-   -- 'static const' attribute of the class.  This works well for
-   -- methods, constants and assorted other elements added manually
-   -- into the class by overrides.
-   if select('#', ...) > 0 then
-      error(("%s: `%s' is not writable"):format(self._name, symbol), 4)
-   end
-   return element
-end
-
--- Keyword translation dictionary.  Used for translating Lua keywords
--- which might appear as symbols in typelibs into Lua-neutral identifiers.
-local keyword_dictionary = {
-   _end = 'end', _do = 'do', _then = 'then', _elseif = 'elseif', _in = 'in',
-   _local = 'local', _function = 'function', _nil = 'nil', _false = 'false',
-   _true = 'true', _and = 'and', _or = 'or', _not = 'not',
-}
-
--- Retrieves (element, category) pair from given componenttable and
--- instance for given symbol.
-function component_mt:_element(instance, symbol)
-   -- Check keyword translation dictionary.  If the symbol can be
-   -- found there, try to lookup translated symbol.
-   symbol = keyword_dictionary[symbol] or symbol
-
-   -- Check whether symbol is directly accessible in the component.
-   local element = rawget(self, symbol)
-   if element then return element end
-
-   -- Decompose symbol name, in case that it contains category prefix
-   -- (e.g. '_field_name' when requesting explicitely field called
-   -- name).
-   local category, name = string.match(symbol, '^(_.-)_(.*)$')
-   if category and name and category ~= '_access' then
-      -- Check requested category.
-      local cat = rawget(self, category)
-      element = cat and cat[name]
-   elseif string.sub(symbol, 1, 1) ~= '_' then
-      -- Check all available categories.
-      local categories = self._categories or {}
-      for i = 1, #categories do
-	 category = categories[i]
-	 local cat = rawget(self, category)
-	 element = cat and cat[symbol]
-	 if element then break end
-      end
-   end
-   if element then
-      -- Make sure that table-based attributes have symbol name, so
-      -- that potential errors contain the name of referenced
-      -- attribute.
-      if type(element) == 'table' and category == '_attribute' then
-	 element._name = element._name or symbol
-      end
-      return element, category
-   end
-end
-
--- Implementation of attribute accessor.  Attribute is either function
--- to be directly invoked, or table containing set and get functions.
-function component_mt:_access_attribute(instance, element, ...)
-   -- If element is a table, assume that this table contains 'get' and
-   -- 'set' methods.  Dispatch to them, and error out if they are
-   -- missing.
-   if type(element) == 'table' then
-      local mode = select('#', ...) == 0 and 'get' or 'set'
-      if not element[mode] then
-	 error(("%s: cannot %s `%s'"):format(
-		  self._name, mode == 'get' and 'read' or 'write',
-		  element._name or '<unknown>'), 5)
-      end
-      element = element[mode]
-   end
-
-   -- Invoke attribute access function.
-   return element(instance, ...)
-end
-
--- Implementation of record_mt, which is inherited from component_mt
+-- Implementation of record_mt, which is inherited from component
 -- and provides customizations for structures and unions.
-local record_mt = component_mt:clone { '_method', '_field' }
+local record_mt = component.mt:clone { '_method', '_field' }
 
 function record_mt:_element(instance, symbol)
    -- First of all, try normal inherited functionality.
-   local element, category = component_mt._element(self, instance, symbol)
+   local element, category = component.mt._element(self, instance, symbol)
    if element then return element, category end
 
    -- If the record has parent struct, try it there.
@@ -380,7 +138,7 @@ function record_mt:_new(fields)
 end
 
 -- Implementation of interface_mt.
-local interface_mt = component_mt:clone {
+local interface_mt = component.mt:clone {
    '_virtual', '_property', '_signal', '_method', '_constant'
 }
 
@@ -399,7 +157,7 @@ function interface_mt:is_type_of(instance)
    return false
 end
 
--- Implementation of class_mt, inherited from component_mt and
+-- Implementation of class_mt, inherited from component and
 -- providing basic class functionality.  Note that signals and
 -- properties are implemented later on GObject descendants only.
 local class_mt = interface_mt:clone {
@@ -410,7 +168,7 @@ local class_mt = interface_mt:clone {
 -- implemented interfaces.
 function class_mt:_resolve(recursive)
    -- Resolve itself using inherited implementation.
-   component_mt._resolve(self)
+   component.mt._resolve(self)
 
    -- Go to parent and implemented interfaces and resolve them too.
    if recursive then
@@ -428,7 +186,7 @@ end
 -- interfaces if element cannot be found in current typetable.
 function class_mt:_element(instance, symbol)
    -- Check default implementation.
-   local element, category = component_mt._element(self, instance, symbol)
+   local element, category = component.mt._element(self, instance, symbol)
    if element then return element, category end
 
    -- Check parent and all implemented interfaces.
@@ -484,19 +242,6 @@ end
 function class_mt:_new()
    -- Create the object.
    return object_new(self._gtype, {})
-end
-
--- Creates new component and sets up common parts according to given
--- info.
-local function create_component(info, mt)
-   -- Fill in meta of the compound.
-   local component = { _name = info.fullname }
-   if info.gtype then
-      -- Bind component in repo, make the relation using GType.
-      component._gtype = info.gtype
-      repo[info.gtype] = component
-   end
-   return setmetatable(component, mt)
 end
 
 -- Table containing loaders for various GI types, indexed by
@@ -585,8 +330,8 @@ end
 -- Loads structure information into table representing the structure
 local function load_record(info)
    local record = create_component(info, record_mt)
-   record._method = get_category(info.methods, core.callable.new)
-   record._field = get_category(info.fields)
+   record._method = component.get_category(info.methods, core.callable.new)
+   record._field = component.get_category(info.fields)
 
    -- Check, whether global namespace contains 'constructor' method,
    -- i.e. method which has the same name as our record type (except
@@ -618,7 +363,7 @@ function typeloader.union(namespace, info)
 end
 
 local function load_properties(info)
-   return get_category(
+   return component.get_category(
       info.properties, nil,
       function(name) return string.gsub(name, '_', '%-') end,
       function(name) return string.gsub(name, '%-', '_') end)
@@ -642,16 +387,16 @@ end
 
 function typeloader.interface(namespace, info)
    -- Load all components of the interface.
-   local interface = create_component(info, interface_mt)
+   local interface = component.create(info, interface_mt)
    interface._property = load_properties(info)
-   interface._method = get_category(info.methods, load_method)
-   interface._signal = get_category(info.signals, nil, load_signal_name,
-				     load_signal_name_reverse)
-   interface._constant = get_category(info.constants, core.constant)
+   interface._method = component.get_category(info.methods, load_method)
+   interface._signal = component.get_category(
+      info.signals, nil, load_signal_name, load_signal_name_reverse)
+   interface._constant = component.get_category(info.constants, core.constant)
    local type_struct = info.type_struct
    if type_struct then
-      interface._virtual = get_category(info.vfuncs, nil, load_vfunc_name,
-					load_vfunc_name_reverse)
+      interface._virtual = component.get_category(
+	 info.vfuncs, nil, load_vfunc_name, load_vfunc_name_reverse)
       interface._class = load_record(type_struct)
    end
    interface._new = find_constructor(info)
@@ -670,19 +415,19 @@ function typeloader.object(namespace, info)
 
    -- Create class instance, copy mt from parent, if parent exists,
    -- otherwise defaults to class_mt.
-   local class = create_component(
+   local class = component.create(
       info, parent and getmetatable(parent) or class_mt)
    class._parent = parent
    class._property = load_properties(info)
-   class._method = get_category(info.methods, load_method)
-   class._signal = get_category(info.signals, nil,
-				 load_signal_name, load_signal_name_reverse)
-   class._constant = get_category(info.constants, core.constant)
-   class._field = get_category(info.fields)
+   class._method = component.get_category(info.methods, load_method)
+   class._signal = component.get_category(
+      info.signals, nil, load_signal_name, load_signal_name_reverse)
+   class._constant = component.get_category(info.constants, core.constant)
+   class._field = component.get_category(info.fields)
    local type_struct = info.type_struct
    if type_struct then
-      class._virtual = get_category(info.vfuncs, nil, load_vfunc_name,
-				    load_vfunc_name_reverse)
+      class._virtual = component.get_category(
+	 info.vfuncs, nil, load_vfunc_name, load_vfunc_name_reverse)
       class._class = load_record(type_struct)
       if parent then class._class._parent = parent._class end
    end
@@ -725,7 +470,7 @@ function namespace_mt:__index(symbol)
    end
 
    -- Check, whether symbol is already loaded.
-   val = component_mt._element(self, nil, symbol, namespace_mt._categories)
+   val = component.mt._element(self, nil, symbol, namespace_mt._categories)
    if val then return val end
 
    -- Lookup baseinfo of requested symbol in the GIRepository.
