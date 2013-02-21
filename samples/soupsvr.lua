67cd64601280cb5083df47541757b04c4de92047
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2013-02-21 22:33:04 +0100

    Add some comments to soupsvr sample

--- samples/soupsvr.lua
+++ samples/soupsvr.lua
@@ -32,31 +32,49 @@ function app:on_activate()
    server:add_handler('/', function(server, msg, path, query, ctx)
       local stream = Gio.File.new_for_path(path:sub(2)):read()
       if stream then
+	 -- The whole is send by function running in coroutine.
+	 -- Coroutine yields when it waits either for data from the
+	 -- disk or signal that chunk was successfully sent.
 	 local next_chunk = coroutine.wrap(function()
 	    local buffer = bytes.new(4096)
 	    while true do
+	       -- Read another chunk of data from the source to the
+	       -- buffer.
 	       stream:read_async(buffer, #buffer, GLib.PRIORITY_DEFAULT, nil,
 				 coroutine.running())
 	       local size = stream.read_finish(coroutine.yield())
 	       if size < 0 then
+		  -- IO error reading disk, this simply shuts our toy
+		  -- server down.
 		  server:quit()
 		  app:release()
 		  return
 	       end
+
+	       -- Send the chunk to the message body.
 	       msg.response_body:append(tostring(buffer):sub(1, size))
 	       server:unpause_message(msg)
+
+	       -- Wait until soup signalizes that chunk was written.
 	       coroutine.yield()
 	       if (size < #buffer) then break end
 	    end
 	    msg.response_body:complete()
 	 end)
 
+	 -- Prepare sending using chunked method.
 	 msg.status_code = 200
 	 msg.response_headers:set_encoding('CHUNKED')
-	 msg.on_wrote_headers = next_chunk
-	 msg.on_wrote_chunk = next_chunk
 
+	 -- When headers are written, start writing body by initial
+	 -- resuming of sending coroutine.
+	 msg.on_wrote_headers = next_chunk
+
+	 -- When the chunk is sent, resume coroutine so that it starts
+	 -- reading and sending another chunk.
+	 msg.on_wrote_chunk = next_chunk
       else
+	 -- File was not found, send proper code.
 	 msg.status_code = 404
 	 msg.response_body:complete()
       end
