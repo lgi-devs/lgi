fe8d08b270d45d516b479048e25b7749faaad23c
Pavel Holejsovsky <pavel.holejsovsky@gmail.com>
2013-11-17 20:08:07 +0100

    Consider additional case for record ctors
    Till now, only 'Namespace.record' and 'Namespace.Record.new' were
    considered.  GLib.String has 'GLib.string_new', so add
    'Namespace.record_new' form too.

--- lgi/record.lua
+++ lgi/record.lua
@@ -145,7 +145,7 @@ function record.load(info)
    -- that type is in CamelCase, while method is
    -- under_score_delimited).  If not found, check for 'new' method.
    local func = info.name:gsub('([%l%d])([%u])', '%1_%2'):lower()
-   local ctor = gi[info.namespace][func]
+   local ctor = gi[info.namespace][func] or gi[info.namespace][func .. '_new']
    if not ctor then ctor = info.methods.new end
 
    -- Check, whether ctor is valid.  In order to be valid, it must
